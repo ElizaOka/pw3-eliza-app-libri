7339bc2a02dae906749e74c680bf9d61e84f2e17
elizaOka <elizkooo@yahoo.com.br>
2024-08-01 22:21:40 -0300

    Limpeza dos arquivos css e criação do primeiro componente

---
+++ pw3-eliza-app-libri/src/components/CardBooks.jsx
@@ -0,0 +1,10 @@
+const CardBooks = (props)=>{
+    return (
+        <div>
+            <h3> {props.titulo}</h3>
+            <p> {props.autor}</p>
+        </div>
+    )
+}
+
+export default CardBooks
