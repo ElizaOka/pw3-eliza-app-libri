9056d039d5f723772e056ba8b51bf5428054b018
ElizaOka <elizkooo@yahoo.com.br>
2024-08-29 22:26:04 -0300

    criação de links e estilização

--- pw3-eliza-app-libri/src/App.jsx
+++ pw3-eliza-app-libri/src/App.jsx
@@ -4,14 +4,35 @@ import './App.css'
 
 /*import CardBooks from './components/CardBooks'*/
 import NavBar from './components/layout/NavBar'
-import capaLivro from './assets/livros/cavernas_aco.jpg'
+
+/* IMPORTA O COMPONENTE DE <CONTEINER*/
+import Container from './components/layout/Container'
+
+import Home from './components/pages/Home'
+import ListBooks from './components/pages/ListBooks'
+import CreateBooks from './components/pages/CreateBooks'
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 
 function App() {
   
 
   return (
     <>
-      <NavBar/>
+      {/* estrutura de navegação */}
+      <BrowserRouter>
+      <Container>
+        <Routes>
+          <Route path='/' element={<NavBar/>}>
+            <Route path= '/'  element ={<Home/>}/>
+              <Route path='/listBook' element ={<ListBooks/>}/>
+              <Route path='/createBook' element ={<createBooks/>}/>
+
+          </Route>
+        </Routes>
+      </Container>
+      </BrowserRouter>
       
     </>
   )
